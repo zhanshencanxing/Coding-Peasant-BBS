25dba391fb4f89f3df2904fbf657f99f20d1fdae
zhanshencanxing <34231561+zhanshencanxing@users.noreply.github.com>
2019-05-15 22:16:32 +0800

    Vue引入组件
    引入核心Vue、Vue路由、Vue状态管理方案、Element-UI、v-charts、axios、qs、vue-quill-editor富文本编辑器

---
+++ main.js
@@ -0,0 +1,36 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VCharts from 'v-charts'
+import axios from 'axios'
+import qs from 'qs'
+
+//引入编辑器组件
+import VueQuillEditor from "vue-quill-editor"
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+//把axios挂载到vue的原型中，在vue中每个组件都可以使用axios发送请求
+Vue.prototype.$axios = axios
+axios.defaults.withCredentials = true
+Vue.prototype.$qs = qs
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(Vuex)
+Vue.use(VCharts)
+Vue.use(VueQuillEditor)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
